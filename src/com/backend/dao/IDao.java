5df4db7848a1ee782f47e6fe79fa5faed9da9adc
Matias Crino <matiascrino@gmail.com>
2023-05-25 16:38:17 -0300

    cargo carpeta al repo, primer commit

---
+++ src/com/backend/dao/IDao.java
@@ -0,0 +1,16 @@
+package com.backend.dao;
+
+import java.util.List;
+
+public interface IDao<T>{
+
+    T guardar(T t);
+
+    List<T> listarTodos();
+
+    void eliminar(int id);
+
+    T buscarPorId(int id);
+
+}
+
